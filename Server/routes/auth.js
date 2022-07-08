d1869fd6ce39ca75f9340c15fd8fae8a897bf049
Byeonminu <ufo3764@naver.com>
2022-07-08 11:09:09 +0000

    feature: sign up, pwd double check, id exist check

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -5,17 +5,33 @@ const bcrypt = require('bcrypt')
 
 /* GET home page. */
 router.post('/register', function (req, res) {
-    console.log("req는: ", req);
-    const encryptedPwd = bcrypt.hashSync(req.body.pwd1, 10);
-    console.log("encryptedpwd : ", encryptedPwd);
 
-    db.query("insert into users (user_id, pwd, email, nickname) values(?, ?, ?, ?)", [req.body.user_id, encryptedPwd, req.body.email, req.body.nickname], (err, result) =>{
-        if(err) throw err;
-        
-       
+    if(req.body.pwd1 != req.body.pwd2){
+        res.json("비밀번호가 같아야 합니다.");
+        return;
+    }
+    
+    db.query("select * from users where user_id = ?", [req.body.user_id], (err1, result1) =>{
+        if(err1) throw err1;
 
-        res.json("데이터 insert 성공");
+        if (result1.length != 0){
+            res.json("이미 존재하는 아이디입니다.");
+            return;
+        }
+        // console.log("req는: ", req);
+        const encryptedPwd = bcrypt.hashSync(req.body.pwd1, 10);
+        console.log("encryptedpwd : ", encryptedPwd);
+        db.query("insert into users (user_id, pwd, email, nickname) values(?, ?, ?, ?)", [req.body.user_id, encryptedPwd, req.body.email, req.body.nickname], (err, result) => {
+            if (err) throw err;
+
+            res.json("데이터 insert 성공");
+        })
+
     })
+
+
+
+  
     
 });
 
