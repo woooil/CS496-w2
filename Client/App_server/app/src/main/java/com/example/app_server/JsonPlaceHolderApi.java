ed70018b583c034622d754d41acdd3b970ab7021
wooil <wooilkim9@gmail.com>
2022-07-08 20:03:27 +0900

    login client

--- Client/App_server/app/src/main/java/com/example/app_server/JsonPlaceHolderApi.java
+++ Client/App_server/app/src/main/java/com/example/app_server/JsonPlaceHolderApi.java
@@ -1,9 +1,13 @@
 package com.example.app_server;
+import android.database.Observable;
+
 import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Field;
 
 public interface JsonPlaceHolderApi {
     @GET("/api")
@@ -11,4 +15,9 @@ public interface JsonPlaceHolderApi {
 
     @POST("/auth/register")
     Call < Object > signup(@Body UserInfo info);
+
+    @POST("/auth/login")
+    @FormUrlEncoded
+    Call < Object > login(@Field("user_id") String user_id,
+                          @Field("password") String password);
 }
