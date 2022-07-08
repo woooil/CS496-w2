ae5b05b7253b049383d107e1233211db810bf87b
wooil <wooilkim9@gmail.com>
2022-07-08 18:50:42 +0900

    commit

--- Client/App_server/app/src/main/java/com/example/app_server/MainActivity.java
+++ Client/App_server/app/src/main/java/com/example/app_server/MainActivity.java
@@ -14,7 +14,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import java.net.HttpURLConnection;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
 public class MainActivity extends AppCompatActivity {
     private TextView signUpTV;
 
