54da4a36f93a804d1063c6ad52f40a9d06384a59
wooil <wooilkim9@gmail.com>
2022-07-08 10:46:28 +0000

    finish login

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,7 +1,7 @@
 var express = require('express');
 const db = require('../model/db');
 var router = express.Router();
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
 /* GET home page. */
 router.post('/register', function (req, res) {
@@ -11,54 +11,32 @@ router.post('/register', function (req, res) {
 
     db.query("insert into users (user_id, pwd, email, nickname) values(?, ?, ?, ?)", [req.body.user_id, encryptedPwd, req.body.email, req.body.nickname], (err, result) =>{
         if(err) throw err;
-        
-       
 
         res.json("데이터 insert 성공");
     })
 });
 
-// password utils
-var genRandomString = function(length) {
-    return crypto.randomBytes(Math.ceil(length/2))
-      .toString('hex')
-      .slice(0, length);
-  };
-  
-var sha512 = function(password, salt) {
-    var hash = crypto.createHmac('sha512', salt);
-    hash.update(password);
-    var value = hash.digest('hex');
-    return {
-        salt: salt,
-        passwordHash: value
-    };
-}
-
-function saltHashPassword(userPassword) {
-    var salt = genRandomString(16);
-    var passwordData = sha512(userPassword, salt);
-    return passwordData;
-}
-
-router.get('/login/', (req, res, next) => {
+router.post('/login', (req, res, next) => {
+    console.log("sssss");
     var post_data = req.body;
     var user_id = post_data.user_id;
     var user_password = post_data.password;
-    var email = post_data.email;
-    var nickname = post_data.nickname;
+    console.log('login: ', req);
     
-    db.query('select * from users where id=?', [user_id], function(err, result, fields) {
+    db.query('select * from users where user_id=?', [user_id], function(err, result, fields) {
         db.on('error', function(err) {
             throw err;
         });
         
         if (result && result.length) {
-            var right_password = result[0].pwd;
-            if (user_password === right_password) 
-                res.end(JSON.stringify(result[0]));
-            else
-                res.end(JSON.stringify('Wrong password!'));
+            bcrypt.compare(user_password, result[0].pwd, (err, result) => {
+                if(err) throw err;
+                if (result == true) 
+                    // res.json('WOWOWOWOW');
+                    res.end(JSON.stringify(result[0]));
+                else
+                    res.end(JSON.stringify('Wrong password!'));
+            });
         }
         else {
             res.json('User not exists!');
