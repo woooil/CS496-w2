7c5fbf8504f04a3a6726fe7b11ce4ad6e92c0c15
wooil <wooilkim9@gmail.com>
2022-07-10 19:22:57 +0900

    manage room status

--- Client/App_server/app/src/main/java/com/example/app_server/RoomActivity.java
+++ Client/App_server/app/src/main/java/com/example/app_server/RoomActivity.java
@@ -13,15 +13,20 @@ import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.slider.RangeSlider;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
@@ -30,15 +35,19 @@ import petrov.kristiyan.colorpicker.ColorPicker;
 
 public class RoomActivity extends AppCompatActivity {
   
-  private TextView nicknameTV, msgBox, roomTV;
+  private TextView msgBox, roomTV;
+  private LinearLayout[] plLL = new LinearLayout[4];
+  private TextView[] plIcon = new TextView[4];
+  private TextView[] plChat = new TextView[4];
   private EditText msgET;
-  private Button logoutBT, msgBT;
+  private Button msgBT, startBT;
   private UsersModal user;
   private final String BASE_URL = "http://192.249.18.196";
   private String room;
   private DrawView paint;
   private ImageButton save, color, stroke, undo;
   private RangeSlider rangeSlider;
+  private Socket socket;
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -54,8 +63,6 @@ public class RoomActivity extends AppCompatActivity {
     }
     
     user = SharedPrefManager.getInstance(this).getUser();
-    nicknameTV = findViewById(R.id.nicknameTV);
-    logoutBT = findViewById(R.id.logoutBT);
     msgBT = findViewById(R.id.msgBT);
     msgET = findViewById(R.id.msgET);
     msgBox = findViewById(R.id.msgBox);
@@ -66,19 +73,52 @@ public class RoomActivity extends AppCompatActivity {
     save = (ImageButton) findViewById(R.id.btn_save);
     color = (ImageButton) findViewById(R.id.btn_color);
     stroke = (ImageButton) findViewById(R.id.btn_stroke);
+    startBT = findViewById(R.id.startBT);
     
-    nicknameTV.setText(user.getNickname() + "!");
+    plLL[0] = findViewById(R.id.p1LL);
+    plLL[1] = findViewById(R.id.p2LL);
+    plLL[2] = findViewById(R.id.p3LL);
+    plLL[3] = findViewById(R.id.p4LL);
+    
+    plIcon[0] = findViewById(R.id.p1Icon);
+    plIcon[1] = findViewById(R.id.p2Icon);
+    plIcon[2] = findViewById(R.id.p3Icon);
+    plIcon[3] = findViewById(R.id.p4Icon);
+    plIcon[0].setText(user.getNickname());
+    
+    plChat[0] = findViewById(R.id.p1Chat);
+    plChat[1] = findViewById(R.id.p2Chat);
+    plChat[2] = findViewById(R.id.p3Chat);
+    plChat[3] = findViewById(R.id.p4Chat);
+    
     roomTV.setText(room);
     
-    logoutBT.setOnClickListener(v -> {
-      SharedPrefManager.getInstance(this).logout();
-    });
-    
-    final Socket socket = IO.socket(URI.create(BASE_URL));
+    socket = IO.socket(URI.create(BASE_URL));
     socket.connect();
     Log.d(null, "socket created!");
     socket.emit("hello", user.getNickname());
     Log.d(null, "socket emitted!");
+    
+    socket.on("information", new Emitter.Listener() {
+      @Override
+      public void call(Object... args) {
+        JSONObject data = (JSONObject) args[0];
+        try {
+          JSONArray all_player = (JSONArray) data.get("all_player");
+          List<String> exampleList = new ArrayList<String>();
+          for (int i = 0; i < all_player.length(); i++) {
+            exampleList.add(all_player.getString(i));
+          }
+          int size = exampleList.size();
+          String[] players = exampleList.toArray(new String[size]);
+          String artist = data.get("artist").toString();
+          Log.d(null, "INFODD: " + players + " AND " + artist);
+          updatePlayers(players, artist);
+        } catch (JSONException e) {
+          e.printStackTrace();
+        }
+      }
+    });
     
     String username = user.getNickname().toString();
     JSONObject new_user = new JSONObject();
@@ -140,6 +180,14 @@ public class RoomActivity extends AppCompatActivity {
       public void onClick(View view) {
         socket.emit("chatMessage", msgET.getText().toString());
         msgET.setText("");
+      }
+    });
+    
+    startBT.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        socket.emit("gameStart");
+        startBT.setVisibility(View.INVISIBLE);
       }
     });
   
@@ -214,6 +262,41 @@ public class RoomActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
       }
     });
+  }
+  
+  @Override
+  public void onBackPressed() {
+    super.onBackPressed();
+    socket.disconnect();
+  }
+  
+  private void updatePlayers(String[] players, String artist) {
+    int size = players.length;
+    int userPosition = Arrays.asList(players).indexOf(user.getNickname());
+    String temp = players[userPosition];
+    players[userPosition] = players[0];
+    players[0] = temp;
+    
+    runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        for (int i = 1; i < size; i++) {
+          plLL[i].setVisibility(View.VISIBLE);
+          plIcon[i].setText(players[i]);
+        }
+        for (int i = size; i < 4; i++) {
+          plLL[i].setVisibility(View.INVISIBLE);
+          plIcon[i].setText("");
+        }
+  
+        if (user.getNickname().equals(artist)) {
+          startBT.setVisibility(View.VISIBLE);
+        } else {
+          startBT.setVisibility(View.INVISIBLE);
+        }
+      }
+    });
+  
   }
   
 
